e3d942a1f767520b475f9ccd062ce3624d479b8b
Matt Hoffman <16hoffma@gmail.com>
2019-10-07 17:19:31 -0400

    E08 Deliverable

--- E08 - Projections/AppClass.cpp
+++ E08 - Projections/AppClass.cpp
@@ -54,21 +54,28 @@ void Application::Display(void)
 		break;
 	case 2:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
 		break;
 	case 3:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_X * 20, ZERO_V3, AXIS_Z * -1);
 		break;
 	case 4:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * -15, ZERO_V3, AXIS_Y);
 		break;
 	case 5:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * -10.21f, ZERO_V3, AXIS_Y);
 		break;
 	case 6:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * 0.0001f, ZERO_V3, AXIS_Y);
+		m_pCamera->SetPerspective(false);
 		break;
 	case 7:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * 10, ZERO_V3, AXIS_Y * -1);
 		break;
 	}
 
